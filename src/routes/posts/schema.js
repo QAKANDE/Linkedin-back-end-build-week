985361f0f9d5c3981cf415b2b2a6c6a17aea0efc
erkeen <63852123+erkeen@users.noreply.github.com>
2020-07-21 16:00:41 +0500

    updated posts schema

--- src/routes/posts/schema.js
+++ src/routes/posts/schema.js
@@ -4,7 +4,7 @@ const postSchema = new Schema(
   {
     text: { type: String },
     username: { type: String },
-    user: { type: Schema.Types.ObjectId, ref: "profile", required: true },
+    user: { type: Schema.Types.String, ref: "profile", required: true },
     image: { type: String, default: "www.www.www" },
   },
   { timestamps: true }
